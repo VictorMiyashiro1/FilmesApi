415a78ecce966646cb3055efc3158b586c99cb1a
Victor Hugo Campos <vh.miyashiro@gmail.com>
2025-08-09 22:36:01 -0300

    swagger

--- Filmes.Api/Program.cs
+++ Filmes.Api/Program.cs
@@ -1,5 +1,6 @@
 using Filmes.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+});
 
 
 
@@ -34,11 +38,12 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    c.RoutePrefix = "swagger"; // URL final: /swagger
+});
 
 app.UseHttpsRedirection();
 
