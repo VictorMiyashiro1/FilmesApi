e5a0eb919e9025c333b4ab7292921f1ce75b8fa1
Victor Hugo Campos <vh.miyashiro@gmail.com>
2025-08-09 23:07:21 -0300

    fix model

--- Filmes.Api/Models/Filmes.cs
+++ Filmes.Api/Models/Filmes.cs
@@ -6,19 +6,22 @@ namespace Filmes.Api.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required, StringLength(200)]
         public string Title { get; set; } = string.Empty;
 
-        [Required]
+        [Required, StringLength(100)]
         public string Genre { get; set; } = string.Empty;
 
+        [StringLength(2000)]
         public string? Description { get; set; }
 
-        [Required]
+        [Url, StringLength(1000)]
         public string? ImageUrl { get; set; }
 
+        [Range(0, int.MaxValue)]
         public int Likes { get; set; }
 
+        [Range(0, int.MaxValue)]
         public int Dislikes { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
